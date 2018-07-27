86f606f1d9b6d21247be81a972b223fe22671386
KUNAL <kumar.kunal@capgemini.com>
2018-07-27 09:49:49 +0530

    first commit

---
+++ src/main/java/org/capgemini/dao/ProductDaoImpl.java
@@ -0,0 +1,19 @@
+package org.capgemini.dao;
+
+import org.capgemini.dto.Product;
+
+public class ProductDaoImpl implements ProductDao{
+
+	
+	public boolean addProduct(Product product) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public Product findProduct(int productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
